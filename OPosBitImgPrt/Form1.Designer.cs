83f7aaea3142fad8f94ebfbd986a477c14888242
yama-ten <tenshi@tokachi-lab.com>
2022-10-23 13:07:55 +0900

    OPosBitImgPrt

---
+++ OPosBitImgPrt/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace OPosBitImgPrt
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btn_open = new System.Windows.Forms.Button();
+			this.btn_close = new System.Windows.Forms.Button();
+			this.cmb_prtName = new System.Windows.Forms.ComboBox();
+			this.btn_hellow = new System.Windows.Forms.Button();
+			this.txt_open_result = new System.Windows.Forms.TextBox();
+			this.btn_dotImage = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btn_open
+			// 
+			this.btn_open.Location = new System.Drawing.Point(15, 41);
+			this.btn_open.Name = "btn_open";
+			this.btn_open.Size = new System.Drawing.Size(121, 23);
+			this.btn_open.TabIndex = 0;
+			this.btn_open.Text = "OPEN";
+			this.btn_open.UseVisualStyleBackColor = true;
+			this.btn_open.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btn_close
+			// 
+			this.btn_close.Location = new System.Drawing.Point(15, 216);
+			this.btn_close.Name = "btn_close";
+			this.btn_close.Size = new System.Drawing.Size(121, 23);
+			this.btn_close.TabIndex = 1;
+			this.btn_close.Text = "CLOSE";
+			this.btn_close.UseVisualStyleBackColor = true;
+			this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+			// 
+			// cmb_prtName
+			// 
+			this.cmb_prtName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmb_prtName.FormattingEnabled = true;
+			this.cmb_prtName.Location = new System.Drawing.Point(15, 15);
+			this.cmb_prtName.Name = "cmb_prtName";
+			this.cmb_prtName.Size = new System.Drawing.Size(121, 20);
+			this.cmb_prtName.TabIndex = 2;
+			// 
+			// btn_hellow
+			// 
+			this.btn_hellow.Location = new System.Drawing.Point(15, 82);
+			this.btn_hellow.Name = "btn_hellow";
+			this.btn_hellow.Size = new System.Drawing.Size(121, 23);
+			this.btn_hellow.TabIndex = 3;
+			this.btn_hellow.Text = "Prinr \'Hellow\'";
+			this.btn_hellow.UseVisualStyleBackColor = true;
+			this.btn_hellow.Click += new System.EventHandler(this.btn_hellow_Click);
+			// 
+			// txt_open_result
+			// 
+			this.txt_open_result.Location = new System.Drawing.Point(142, 43);
+			this.txt_open_result.Name = "txt_open_result";
+			this.txt_open_result.ReadOnly = true;
+			this.txt_open_result.Size = new System.Drawing.Size(255, 19);
+			this.txt_open_result.TabIndex = 4;
+			// 
+			// btn_dotImage
+			// 
+			this.btn_dotImage.Location = new System.Drawing.Point(15, 111);
+			this.btn_dotImage.Name = "btn_dotImage";
+			this.btn_dotImage.Size = new System.Drawing.Size(121, 23);
+			this.btn_dotImage.TabIndex = 5;
+			this.btn_dotImage.Text = "Print Dot Image";
+			this.btn_dotImage.UseVisualStyleBackColor = true;
+			this.btn_dotImage.Click += new System.EventHandler(this.btn_dotImage_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(426, 251);
+			this.Controls.Add(this.btn_dotImage);
+			this.Controls.Add(this.txt_open_result);
+			this.Controls.Add(this.btn_hellow);
+			this.Controls.Add(this.cmb_prtName);
+			this.Controls.Add(this.btn_close);
+			this.Controls.Add(this.btn_open);
+			this.Name = "Form1";
+			this.Text = "OposBitmapImagePrint";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btn_open;
+		private System.Windows.Forms.Button btn_close;
+		private System.Windows.Forms.ComboBox cmb_prtName;
+		private System.Windows.Forms.Button btn_hellow;
+		private System.Windows.Forms.TextBox txt_open_result;
+		private System.Windows.Forms.Button btn_dotImage;
+	}
+}
+
---
+++ OPosBitImgPrt/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.PointOfService;
+using OposPOSPrinter_CCO;
+
+//namespace Microsoft.PointOfService
+//{
+//	public enum ErrorCode
+//	{
+//		Success = 0,
+//		Closed = 101,
+//		Claimed = 102,
+//		NotClaimed = 103,
+//		NoService = 104,
+//		Disabled = 105,
+//		Illegal = 106,
+//		NoHardware = 107,
+//		Offline = 108,
+//		NoExist = 109,
+//		Exists = 110,
+//		Failure = 111,
+//		Timeout = 112,
+//		Busy = 113,
+//		Extended = 114,
+//		Deprecated = 115
+//	}
+
+//  public enum PrinterStation
+//  {
+//  	None = 0,		//     The current printer station is undefined.
+//  	Journal = 1,	//     The current printer station is Journal.
+//  	Receipt = 2,	//     The current printer station is Receipt.
+//  	Slip = 4,		//     The current printer station is Slip.
+//  	TwoReceiptJournal = 32771,	//     The current printer station combines Receipt and Journal.
+//  	TwoSlipJournal = 32773,		//     The current printer station combines Slip and Journal.
+//  	TwoSlipReceipt = 32774		//     The current printer station combines Receipt and Slip.
+//  }
+//}
+
+namespace OPosBitImgPrt
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			string[] prt_name = new string[]{"TM-T88IIIM","TM-T88IVM","TM-T88VM","TM-T88VIM","TM-T88VIIM" };
+
+			cmb_prtName.Items.AddRange(prt_name);
+			cmb_prtName.SelectedIndex = 0;
+
+			buttons_enable(false);
+		}
+
+//		public OPOSPOSPrinter PosPrt;
+//		public OposPOSPrinter_CCO.OPOSPOSPrinterClass PosPrt;
+		public OposPOSPrinter_CCO.OPOSPOSPrinter PosPrt;
+
+		void buttons_enable(bool sw) {
+			btn_close.Enabled =
+			btn_dotImage.Enabled = 
+			btn_hellow.Enabled = sw;
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var prt = cmb_prtName.SelectedItem.ToString();
+
+			PosPrt = new OPOSPOSPrinter();
+			//if (PosPrt.Open(prt) == Microsoft.PointOfService.ErrorCode.Success)
+			ErrorCode result;
+			
+			result = (ErrorCode)PosPrt.Open(prt);
+			if (result == ErrorCode.Success) {
+				result = (ErrorCode)PosPrt.ClaimDevice(1000);
+				if (result == ErrorCode.Success) {
+					PosPrt.DeviceEnabled = true;
+
+					if (PosPrt.DeviceEnabled) {
+						buttons_enable(true);
+					}
+
+				} else {
+					PosPrt.Close();
+				}
+			}
+			txt_open_result.Text = $"{(int)result}:{result}";
+
+			if (result != ErrorCode.Success) {
+				btn_open.Enabled = 
+				cmb_prtName.Enabled = true;
+
+				btn_close.Enabled = 
+				btn_hellow.Enabled = false;
+			}
+		}
+
+		private void btn_close_Click(object sender, EventArgs e)
+		{
+			if (PosPrt != null && PosPrt.OpenResult == (int)ErrorCode.Success) {
+				PosPrt.Close();
+
+				buttons_enable(false);
+
+				btn_open.Enabled = 
+				cmb_prtName.Enabled = true;
+			}
+		}
+
+		private void btn_hellow_Click(object sender, EventArgs e)
+		{
+			PosPrt.PrintNormal((int)PrinterStation.Receipt, "Hellow Pos Printer. \n");
+		}
+
+		private void btn_dotImage_Click(object sender, EventArgs e)
+		{
+			bit_image_test();
+		}
+
+
+// ---
+		string cvt_bit_image(int mode, int width, byte[] bit_img)
+		{
+			byte[] header = new byte[16];
+			int hp = 0;
+			header[hp++] = (byte)'\x1b';
+			header[hp++] = (byte)'\x2a';
+			header[hp++] = (byte)mode; // image-mode 0:8single,1:8double, 32:24single,33:24:double
+			header[hp++] = (byte)(width%256); // size-low
+			header[hp++] = (byte)(width/256); // size-hight
+ 
+			string data= "";
+			string dbg_text= "";
+
+			for (int i=0; i<hp; i++) {
+				int c = header[i];
+				char c1 = (char)(c/16 + (int)'0');
+				char c2 = (char)(c%16 + (int)'0');
+				data += new string(c1,1) + new string(c2,1);
+				dbg_text += $"{c:x}"; // -- debug text	
+			}
+ 
+			int size = width;			
+			if (mode==32 || mode==33) {	// 24 dot image 
+				size = width * 3;
+			}
+ 
+			for (int i=0; i<size; i++) {
+				int c = bit_img[i];
+				data += new string((char)((int)'0'+(c/16)),1) + new string((char)((int)'0'+(c%16)),1);
+				dbg_text += $"{c:x}"; // -- debug text	
+			}
+ 
+			return data;
+		}
+ 
+ 
+		int prt_bit_image(int mode, int width, byte[] bit_img)
+		{
+			var cur_cnvMode = PosPrt.BinaryConversion;
+			PosPrt.BinaryConversion = (int)BinaryConversion.Nibble;
+ 
+			string data = cvt_bit_image(mode, width, bit_img);
+			int stat = PosPrt.PrintNormal((int)PrinterStation.Receipt, data);
+			
+			PosPrt.BinaryConversion = cur_cnvMode;
+ 
+			return stat;
+		}
+ 
+		void bit_image_test() {
+			//--- bit-image
+			PosPrt.PrintNormal( (int)PrinterStation.Receipt, "bit-image\r\n");
+			byte[] b_img = new byte[4096];
+			int width = 512;
+ 
+			uint dot = 0;
+			// 8dot
+			for (int i=0; i<width; i++) {
+	 //			b_img[i] = i;
+				b_img[i] = (byte)(dot & 0xff);
+				dot <<= 1;
+				dot |= 1;
+				if (dot > 0x0ff) 
+					dot = 0;
+			}
+			prt_bit_image(0, width, b_img);
+			PosPrt.PrintNormal( (int)PrinterStation.Receipt, "\r\n8dot end-image\r\n");
+ 
+			prt_bit_image(1, width, b_img);
+			PosPrt.PrintNormal( (int)PrinterStation.Receipt, "\r\n8dot-W end-image\r\n");
+ 
+			// 24dot
+			dot = 1;
+			for (int i=0; i<width*3; i+=3) {
+				b_img[i]   = (byte)((dot & 0xff0000) >> 16);
+				b_img[i+1] = (byte)((dot & 0x00ff00) >> 8);
+				b_img[i+2] = (byte)(dot & 0xff);
+				dot <<= 1;
+				dot |= 1;
+				if (dot > 0x0ffffff) 
+					dot = 1;
+			}
+			prt_bit_image(32, width, b_img);
+			PosPrt.PrintNormal( (int)PrinterStation.Receipt, "\r\n24dot end-image\r\n");
+ 
+			prt_bit_image(33, width, b_img);
+			PosPrt.PrintNormal( (int)PrinterStation.Receipt, "\r\n24dot-W end-image\r\n");
+		}
+
+		// ---
+	}
+}
